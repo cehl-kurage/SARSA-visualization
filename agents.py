8b93dd0ef3927555e239067bbda8711cd4977369
イシヒラ <83456982+Ishihira@users.noreply.github.com>
2023-08-24 10:27:49 +0900

    -m

---
+++ agents.py
@@ -0,0 +1,103 @@
+# SARSAを実装しましたが、実装が誤っているか、Qtableの更新が上手くいっていないかで学習が上手くいっていません。
+import random
+from time import sleep
+
+import numpy as np
+
+from maze import Direction, Maze
+
+DIRECTIONS = (Direction.UP, Direction.DOWN, Direction.LEFT, Direction.RIGHT)
+INDEX2DIRECTIONS = {i: value for i, value in enumerate(DIRECTIONS)}
+
+
+class Agent:
+    def __init__(
+        self,
+        env: Maze,
+        alpha: float = 0.5,
+        gamma: float = 0.99,
+        max_initial_q: float = 0.1,
+    ):
+        """Agent for exploring maze environment.
+
+        Args:
+            env (Maze): Maze environment.
+            alpha (float, optional): . Defaults to 0.5.
+            gamma (float, optional): _description_. Defaults to 0.99.
+            max_initial_q (float, optional): _description_. Defaults to 0.1.
+        """
+        self.env = env
+        self.alpha = alpha
+        self.gamma = gamma
+        self.qtable = np.random.uniform(
+            low=-max_initial_q, high=-max_initial_q, size=(*self.env.maze.shape, 4)
+        )
+
+    def update_qtable(self, state, action, reward, next_state, next_action):
+        current_q_index = (*state, action)
+        next_q_index = (*next_state, next_action)
+        self.qtable[current_q_index] += self.alpha * (
+            reward
+            + self.gamma * self.qtable[next_q_index]
+            - self.qtable[current_q_index]
+        )
+
+    def act_randomly(self):
+        return random.randint(0, len(DIRECTIONS) - 1)
+
+    def act_greedy(self, state):
+        pos = self.qtable[state]
+        action = np.argmax(pos)
+        return action
+
+    def act(self, state, episode):
+        epsilon = 0.7 * (1 / (episode + 1))
+        if epsilon < np.random.uniform(0, 1):
+            action = self.act_greedy(state)
+        else:
+            action = self.act_randomly()
+        return action
+
+
+def train(agent: Agent, num_episodes: int = 200, num_steps: int = 30):
+    for episode in range(num_episodes):
+        state = tuple(agent.env.current_position)
+        action = agent.act(state, episode)
+        episode_reward = 0
+
+        for t in range(num_steps):
+            reward = 0
+            moved_successfully, is_goaled = agent.env.move_to(INDEX2DIRECTIONS[action])
+            next_state = tuple(agent.env.current_position)
+            if is_goaled:
+                reward = 10
+            if not moved_successfully:  # 動いた方向が壁なら、ペナルティ
+                reward = -1
+            episode_reward += reward
+            next_action = agent.act(next_state, episode)
+            agent.update_qtable(state, action, reward, next_state, next_action)
+            state, action = next_state, next_action
+            if is_goaled:
+                break
+        agent.env.reset()
+        if episode % 50 == 0:
+            print(f"Episode {episode} finished / Episode reward: {episode_reward}")
+
+
+if __name__ == "__main__":
+    agent = Agent(Maze())
+    train(agent, num_episodes=50, num_steps=100)
+
+    # If the qtable does not display well, comment out the part that prints the per-episode reward when learning.
+    # print(agent.qtable)
+
+    # input()  # Stop executing to watch q-table.
+    goaled = False
+    agent.env.reset()
+    while not goaled:  # Test the result of training.
+        print(agent.env, "\n")
+        state = tuple(agent.env.current_position)
+        action = agent.act_greedy(state)
+        _, goaled = agent.env.move_to(INDEX2DIRECTIONS[action])
+        sleep(0.5)
+    print("GOAL")
---
+++ maze.py
@@ -0,0 +1,105 @@
+# 仕様？
+# 居られる場所、いられない場所をbool値で示す
+# 移動は標準入力で行う
+# 迷路はnumpyの2d arrayで表現
+# 現在地は特殊な表示
+# mazeクラス
+# up, down, right, leftの各メソッドで動く
+# current_position属性を持っておいて、それに対して各操作をするタイミングで正常性を検証する
+# 正常性のチェックは動くメソッドの外に実装
+
+from copy import deepcopy
+
+import numpy as np
+
+
+class Direction:
+    UP = "up"
+    DOWN = "down"
+    RIGHT = "right"
+    LEFT = "left"
+
+
+class Maze:
+    def __init__(self, size=(5, 5), wall_index=[2, 9, 11, 12, 14, 15, 22, 24], maze=None) -> None:
+        if maze is not None:
+            self.maze = maze
+            walls = np.where(np.logical_not(maze))
+            self.walls = [np.array(i, j) for i, j in zip(walls)]
+        else:
+            flat_maze = np.full(size[0] * size[1], True)
+            wall_index = np.array(wall_index) - 1
+            flat_maze[[wall_index]] = False
+            self.maze = flat_maze.reshape(size)
+            self.walls = [np.array([i // size[0], i % size[0]]) for i in wall_index]
+        self.commands = (Direction.UP, Direction.DOWN, Direction.RIGHT, Direction.LEFT)
+        self.current_position = np.zeros((2,), int)
+
+    def up(self):
+        next_position = deepcopy(self.current_position)
+        next_position[0] -= 1
+        return next_position
+
+    def down(self):
+        next_position = deepcopy(self.current_position)  # こうしないと参照渡しになって4にます。
+        next_position[0] += 1
+        return next_position
+
+    def right(self):
+        next_position = deepcopy(self.current_position)
+        next_position[1] += 1
+        return next_position
+
+    def left(self):
+        next_position = deepcopy(self.current_position)
+        next_position[1] -= 1
+        return next_position
+
+    def move_to(self, command: str) -> bool:
+        command = command.lower()
+        if command not in self.commands:
+            return False, False
+
+        match command:
+            case Direction.UP:
+                next_position = self.up()
+            case Direction.DOWN:
+                next_position = self.down()
+            case Direction.LEFT:
+                next_position = self.left()
+            case Direction.RIGHT:
+                next_position = self.right()
+        is_valid_position = self.is_valid_position(next_position)
+        if is_valid_position:
+            self.current_position = next_position
+        return is_valid_position, self.is_goaled()
+
+    def is_goaled(self):
+        return (self.current_position == (np.array(self.maze.shape) - 1)).all()
+
+    def is_valid_position(self, next_position):
+        return not (
+            (next_position < 0).any()
+            or (next_position >= self.maze.shape).any()
+            or any([(next_position == wall).all() for wall in self.walls])
+        )
+
+    def reset(self):
+        self.current_position = np.zeros((2,), int)
+
+    def __str__(self):
+        current_maze = np.where(self.maze, "□", "■")
+        current_maze[tuple(self.current_position)] = "*"
+        return str(current_maze)
+
+
+if __name__ == "__main__":
+    maze = Maze()
+    goaled = False
+    print(maze)
+    while not goaled:
+        command = input(f"動く方向を{maze.commands}から指定してください: ")
+        moved_successfully, goaled = maze.move_to(command)
+        print(maze)
+    print("!!!!! goal !!!!!")
+    print(maze.maze)
